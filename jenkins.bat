1f89591780fe7e500a7f9620b9e5327c15b86518
Ptombak <kuaybece@gmail.com>
2018-04-24 14:17:38 -0400

    jenkins_job

---
+++ jenkins.bat
@@ -0,0 +1,4 @@
+java -cp C:\Users\CybertekStudent\workspace\jenkins_github_Demo\bin;C:\Selenium_Lib\*;C:\Users\CybertekStudent\.p2\pool\plugins\* org.testng.TestNG testng.xml   
+
+
+ 
---
+++ src/test_package/jenkins_github_integretion.java
@@ -0,0 +1,41 @@
+package test_package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class jenkins_github_integretion {
+
+	WebDriver driver;
+	
+@BeforeTest
+	
+    public void setUp(){
+		
+	 System.setProperty("webdriver.chrome.driver","C:\\Selenium_Lib\\chromedriver.exe");
+	 driver=new ChromeDriver();
+	 driver.get("http://www.google.com");
+	 
+	 
+ }
+	
+ @Test
+     public void mest(){
+ 
+     WebElement Search_box= driver.findElement(By.name("q"));
+   
+    Search_box.sendKeys("car" + Keys.ENTER);
+}
+ @AfterTest
+     public void teardown(){
+	 
+	 driver.quit();
+	 
+ }
+ }
+ 
